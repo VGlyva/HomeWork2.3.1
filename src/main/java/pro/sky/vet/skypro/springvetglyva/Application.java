f0885bf35e55c9d66514df066a09ad9a08f0dc62
VGlyva <135941893+VGlyva@users.noreply.github.com>
2023-10-01 18:32:02 +0700

    Мой первый коммит

---
+++ src/main/java/pro/sky/vet/skypro/springvetglyva/Application.java
@@ -0,0 +1,13 @@
+package pro.sky.vet.skypro.springvetglyva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
